990648ce501ce3cb6bf47ca782a4c58d0c7449e5
unknown <vatocat_nn@mail.ru>
2023-06-03 20:40:39 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { onMounted } from 'vue';
+import {useMainStore} from './stores/main';
+
+  onMounted(() => {
+    const store = useMainStore();
+    store.expand();
+    store.ready();
+  });
+
+  
+</script>
+
+<template>
+<div id="app">
+  <p>TELEGRAM FRONT</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/stores/main.js
@@ -0,0 +1,28 @@
+import { defineStore } from "pinia"
+
+export const useMainStore = defineStore('main', {
+    state: () => (
+        { 
+            tg: window.Telegram.WebApp,
+        }
+    ),
+    getters: {
+      telegram: (state) => state.tg,
+      isExpanded: (state) => state.tg.isExpanded,
+      viewportHeight: (state) => state.tg.viewportHeight,
+    },
+    actions: {
+        close() {
+            this.tg.close();
+        },
+        ready() {
+            this.tg.ready();
+        },
+        expand() {
+            this.tg.expand();
+        },
+        sendData(data) {
+            this.tg.sendData(data);
+        }
+    },
+  })
